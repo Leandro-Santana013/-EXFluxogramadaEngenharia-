30c53a223b032c4d6e7be0c03870e2815ca98fbd
Leandro-Santana013 <ls1393424@gmail.com>
2025-05-17 22:57:03 -0700

    logica

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿char movendo, deveria;
+
+Console.WriteLine("Objeto está se movendo (S/N): ");
+movendo = char.Parse(Console.ReadLine()!.ToUpper());
+
+Console.WriteLine("Deveria (S/N) se mover: ");
+deveria = char.Parse(Console.ReadLine()!.ToUpper());
+
+if (movendo != 'S' && movendo != 'N' || deveria != 'S' && deveria != 'N')
+{
+    Console.WriteLine("Letra errada");
+    return;
+}
+
+if (movendo == 'S')
+{
+    if (deveria == 'S')
+        Console.WriteLine("Otimo");
+    else
+        Console.WriteLine("SilverTape");
+}
+else
+{
+    if (deveria == 'S')
+        Console.WriteLine("WD40");
+    else
+        Console.WriteLine("Otimo");
+}
--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# -EXFluxogramadaEngenharia-
-Faça um programa que implemente o meme _Fluxograma da Engenharia_.
+### Faça um programa que implemente o meme _Fluxograma da Engenharia_.
+
+Exemplos:
+
+```
+--- Fluxograma da Engenharia ---
+
+O objeto está se movendo (S/N)? N
+Deveria? S
+
+Use WD-40.
+```
+
+```
+--- Fluxograma da Engenharia ---
+
+O objeto está se movendo (S/N)? S
+Deveria? N
+
+Use Silver Tape.
+```
+
+```
+--- Fluxograma da Engenharia ---
+
+O objeto está se movendo (S/N)? N
+Deveria? N
+
+Ótimo!
